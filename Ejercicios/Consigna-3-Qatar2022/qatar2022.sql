df6edb0715ce7ec270dd13dbcd9347056439cc97
Jeremias R. Guzman <138630815+JereRG@users.noreply.github.com>
2023-10-05 00:02:33 -0300

    Insertando datos - Qatar2022

--- Ejercicios/Consigna-3-Qatar2022/qatar2022.sql
+++ Ejercicios/Consigna-3-Qatar2022/qatar2022.sql
@@ -64,3 +64,46 @@ CREATE TABLE Hincha (
     PartidoID INT,
     FOREIGN KEY (PartidoID) REFERENCES Partido(ID)
 );
+
+INSERT INTO Estadio (Nombre, Ciudad, CapacidadMaxima, CapacidadHabitada, CantidadPoliciaPorPartido) VALUES
+('Estadio Nacional', 'Ciudad A', 50000, 40000, 100),
+('Estadio Municipal', 'Ciudad B', 30000, 25000, 80),
+('Estadio Metropolitano', 'Ciudad C', 60000, 55000, 120),
+('Estadio Ciudad Deportiva', 'Ciudad D', 35000, 28000, 90),
+('Estadio Olímpico', 'Ciudad E', 70000, 65000, 150);
+
+INSERT INTO Equipo (NombreSeleccion, NombreDT, LugarEntrenamiento, CuerpoTecnico) VALUES
+('Selección A', 'Director Técnico A', 'Campo de entrenamiento A', 'Cuerpo técnico A'),
+('Selección B', 'Director Técnico B', 'Campo de entrenamiento B', 'Cuerpo técnico B'),
+('Selección C', 'Director Técnico C', 'Campo de entrenamiento C', 'Cuerpo técnico C'),
+('Selección D', 'Director Técnico D', 'Campo de entrenamiento D', 'Cuerpo técnico D'),
+('Selección E', 'Director Técnico E', 'Campo de entrenamiento E', 'Cuerpo técnico E');
+
+INSERT INTO Jugador (NumeroCamiseta, PosicionCancha, Nombre, Apellido, Apodo, EquipoNombreSeleccion) VALUES
+(10, 'Delantero', 'Jugador1', 'Apellido1', 'Apodo1', 'Selección A'),
+(7, 'Centrocampista', 'Jugador2', 'Apellido2', 'Apodo2', 'Selección A'),
+(4, 'Defensor', 'Jugador3', 'Apellido3', 'Apodo3', 'Selección B'),
+(11, 'Delantero', 'Jugador4', 'Apellido4', 'Apodo4', 'Selección B'),
+(9, 'Centrocampista', 'Jugador5', 'Apellido5', 'Apodo5', 'Selección C');
+
+INSERT INTO Arbitro (Pasaporte, PaisOrigen, AñoInicioActividad, Nombre, Apellido, ArbitroReemplazoPasaporte) VALUES
+('AR123', 'Argentina', 2000, 'Arbitro1', 'Apellido1', NULL),
+('BR456', 'Brasil', 2005, 'Arbitro2', 'Apellido2', 'AR123'),
+('UR789', 'Uruguay', 2010, 'Arbitro3', 'Apellido3', 'BR456'),
+('CH567', 'Chile', 2015, 'Arbitro4', 'Apellido4', 'AR123'),
+('CO890', 'Colombia', 2018, 'Arbitro5', 'Apellido5', 'BR456');
+
+INSERT INTO Partido (ID, InstanciaTorneo, Duracion, Fecha, ArbitroPasaporte, EstadioNombre, EquipoLocalNombre, EquipoVisitanteNombre) VALUES
+(1, 'Cuartos de final', 90, '2023-10-01 15:00:00', 'AR123', 'Estadio Nacional', 'Selección A', 'Selección B'),
+(2, 'Cuartos de final', 90, '2023-10-02 15:00:00', 'BR456', 'Estadio Municipal', 'Selección C', 'Selección D'),
+(3, 'Semifinal', 90, '2023-10-03 15:00:00', 'UR789', 'Estadio Metropolitano', 'Selección A', 'Selección C'),
+(4, 'Semifinal', 90, '2023-10-04 15:00:00', 'CH567', 'Estadio Ciudad Deportiva', 'Selección B', 'Selección D'),
+(5, 'Final', 120, '2023-10-05 16:00:00', 'CO890', 'Estadio Olímpico', 'Selección A', 'Selección D');
+
+INSERT INTO Hincha (NumeroTicket, Vestimenta, UbicacionZona, UbicacionFila, UbicacionAsiento, MedioTransporte, PartidoID) VALUES
+(1, 'Camiseta roja', 'Tribuna Norte', 3, 15, 'Auto particular', 1),
+(2, 'Camiseta verde', 'Tribuna Sur', 2, 10, 'Autobús', 1),
+(3, 'Camiseta azul', 'Tribuna Este', 4, 20, 'Auto particular', 2),
+(4, 'Camiseta amarilla', 'Tribuna Oeste', 5, 25, 'Tren', 2),
+(5, 'Camiseta blanca', 'Tribuna Norte', 1, 5, 'Caminando', 3);
+
